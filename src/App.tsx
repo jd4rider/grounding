cf84a8b2ef66b589e8abb06be64cc7d7de5002f7
Jonathan Forrider <jd4rider@gmail.com>
2025-06-22 22:46:41 -0600

    3 Commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ function App() {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % steps.length);
   };
 
-
-
   useEffect(() => {
     fetch('/data/groundingSteps.json')
       .then((res) => res.json())
@@ -37,28 +35,6 @@ function App() {
       </Card.Body>
     </Card>
   );
-    /*<>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>*/
 }
 
 export default App
